77e91d6c1aa012a6266b75fbd5407d755cf6845d
Estdimer <115674299+Estdimer@users.noreply.github.com>
2022-10-28 15:10:17 -0300

    Add files via upload

---
+++ src/main/java/Ejercicio/Ejercicios.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicio;
+
+import java.util.Date;
+
+/**
+ *
+ * @author corre
+ */
+public class Ejercicios {
+    public String nombre;
+    public double repeticiones;
+    public String fecha;
+
+    public Ejercicios(String nombre, double repeticiones, String fecha) {
+        this.nombre = nombre;
+        this.repeticiones = repeticiones;
+        this.fecha = fecha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(double calorias) {
+        this.repeticiones = repeticiones;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
