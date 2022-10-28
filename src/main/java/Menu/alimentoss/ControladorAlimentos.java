7422f20178a891634513c5675f471f638fd8dd24
Estdimer <115674299+Estdimer@users.noreply.github.com>
2022-10-28 14:43:55 -0300

    Add files via upload

---
+++ src/main/java/Menu/alimentoss/ControladorAlimentos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Menu.alimentoss;
+
+import Menu.alimentoss.Alimentos;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author corre
+ */
+public class ControladorAlimentos {
+    public static ArrayList<Alimentos> alimentos =new ArrayList<>();
+}
